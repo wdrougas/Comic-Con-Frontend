93964cdd6ae90850735ceff66e2b7f1664479f72
Will Drougas <wdrougas@gmail.com>
2020-01-16 11:28:31 -0500

    components

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainContainer from './MainContainer'
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MainContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/MainContainer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default class MainContainer extends React.Component {
+    render() {
+        return null
+    }
+}
