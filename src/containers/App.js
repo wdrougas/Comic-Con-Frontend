dd14028a08b9405b2b877b648aa3ef36a97784a0
Will Drougas <wdrougas@gmail.com>
2020-01-17 10:49:47 -0500

    destory side bar

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,45 +25,12 @@ componentDidMount = () => {
     
     return (
   
-      <div>
-      <Header className='App' as='h1'>Welcome to ComicCon</Header>
-  <Sidebar.Pushable as={Segment}>
-      <Sidebar
-        as={Menu}
-        animation='overlay'
-        icon='labeled'
-        inverted
-        vertical
-        visible
-        width='thin'
-      >
-        <Menu.Item as='a'>
-          <Icon name='video play' />
-          Login
-        </Menu.Item>
-        <Menu.Item as='a'>
-          <Icon name='video' />
-          Home
-        </Menu.Item>
-        <Menu.Item as='a'>
-          <Icon name='search' />
-          Search
-        </Menu.Item>
-        <Menu.Item as='a'>
-          <Icon name='film' />
-          Favorites
-        </Menu.Item>
-      </Sidebar>
-  
-  
-      
-      <Sidebar.Pusher>
-        <div className='ui text container'>
+    <div>
+      <Header className='App' as='h1'>Welcome to ComicCon</Header>>
+      <div className='ui text container'>
           <br/>
           <AllMovies movies={this.state.movies} />
-        </div>
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
+      </div>
     </div>
     );
   }
